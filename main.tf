693efc737bd4bda831fcdc9e7370fe352c3169f8
M Rohit Singha <42908618+Rohit-01@users.noreply.github.com>
2023-10-21 11:36:46 +0530

    added key-pair main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,22 @@
 resource "aws_instance" "public_instance" {
  ami           = var.ami
  instance_type = var.instance_type
-
+ key_name      = aws_key_pair.capstone_key.capstone-project-keypair
  tags = {
    Name = var.name_tag,
  }
 }
 
 resource "aws_instance" "docker_instance" {
-ami            = var.ami
+ami           = var.ami
 instance_type = var.instance_type
-
+key_name      = aws_key_pair.capstone_key.capstone-project-keypair
 tags = {
    Name = var.name_tag_1,
  }
+}
+
+resource "aws_key_pair" "capstone_key" {
+  key_name   = "capstone-project-keypair"
+  public_key = file("C:/Users/mrohi/.ssh/capstone-project.pem") 
 }
