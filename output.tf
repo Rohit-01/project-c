1a94645709c0bb7673f4309c0dd504d9f599cde4
M Rohit Singha <42908618+Rohit-01@users.noreply.github.com>
2023-10-21 10:31:44 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -7,3 +7,13 @@ output "instance_id" {
  value       = aws_instance.public_instance.id
  description = "Instance ID"
 }
+
+output "public_ip_docker" {
+ value       = aws_instance.docker_instance.public_ip
+ description = "Public IP Address of EC2 instance"
+}
+
+output "instance_id_docker" {
+ value       = aws_instance.docker_instance.id
+ description = "Instance ID"
+}
